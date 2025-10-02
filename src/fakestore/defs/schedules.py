ad80aa283b4c2f85cb1ff8d2b626154b6fb03524
Nurman Hadi <mnurmanhadi02@gmail.com>
2025-10-02 22:29:58 +0700

    update schedules every minutes

--- src/fakestore/defs/schedules.py
+++ src/fakestore/defs/schedules.py
@@ -2,21 +2,21 @@ import dagster as dg
 
 etl_user_schedule = dg.ScheduleDefinition(
     name="etl_user_schedule",
-    cron_schedule="10 10 2 10 *",
+    cron_schedule="* * * * *",
     job_name="etl_user_job",
     execution_timezone="Asia/Jakarta"
 )
 
 etl_product_schedule = dg.ScheduleDefinition(
     name="etl_product_schedule",
-    cron_schedule="9 10 2 10 *",
+    cron_schedule="* * * * *",
     job_name="etl_product_job",
     execution_timezone="Asia/Jakarta"
 )
 
 etl_cart_schedule = dg.ScheduleDefinition(
     name="etl_cart_schedule",
-    cron_schedule="11 10 2 10 *",
+    cron_schedule="* * * * *",
     job_name="etl_cart_job",
     execution_timezone="Asia/Jakarta"
 )
